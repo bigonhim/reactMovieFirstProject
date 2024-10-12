8ac98d1325cce005348b3ed7d9e5e829ea293cc0
Biegon <156933863+Bigoncode@users.noreply.github.com>
2024-10-12 15:45:31 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard";
 import "./App.css";
 import searchIcon from "./search.svg";
 
-const API_URL = "http://www.omdbapi.com?apikey=2a6b464a";
+const API_URL = "https://www.omdbapi.com?apikey=2a6b464a";
 
 
 
